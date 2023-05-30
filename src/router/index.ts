630b3f17a11a209a93761ee45febbcbb79a63304
NicolasET <nicolaset98@gmail.com>
2023-05-30 18:45:20 -0500

    first commit

---
+++ src/router/index.ts
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import FormView from '../views/FormView.vue';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: FormView
+    },
+    {
+      path: '/thank-you',
+      name: 'thankYou',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ThankView.vue')
+    }
+  ]
+});
+
+export default router;
---
+++ src/views/FormView.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { reactive, ref } from 'vue';
+
+const labelPosition = ref('right');
+
+const formLabelAlign = reactive({
+  name: '',
+  region: '',
+  type: ''
+});
+</script>
+
+<template>
+  <div style="margin: 20px" />
+  <el-form
+    :label-position="labelPosition"
+    label-width="100px"
+    :model="formLabelAlign"
+    style="max-width: 460px"
+  >
+    <el-form-item label="Name">
+      <el-input v-model="formLabelAlign.name" />
+    </el-form-item>
+    <el-form-item label="Activity zone">
+      <el-input v-model="formLabelAlign.region" />
+    </el-form-item>
+    <el-form-item label="Activity form">
+      <el-input v-model="formLabelAlign.type" />
+    </el-form-item>
+  </el-form>
+</template>
+
+<style scoped>
+
+</style>
